c2442f72a5a943d3d10d3d2507df0f79156a371b
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-02 23:31:16 -0600

    Add TEMPLATE_DIRS variable.

--- project/project/settings.py
+++ project/project/settings.py
@@ -83,3 +83,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Template directory
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
+
