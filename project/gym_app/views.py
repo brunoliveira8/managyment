a4d610af54a67f7518ec77b02f7f56aa819e0ea8
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-02 22:54:07 -0600

    Add views.index()
    This is the first page of our app. However, it is necessary to mapping
    the url in gym_app/urls.py and then, project/urls.py.

--- project/gym_app/views.py
+++ project/gym_app/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+#This is the First Page's view.
+def index(request):
+    return HttpResponse("Gym Manager System")
