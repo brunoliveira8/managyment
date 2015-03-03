8df7f73b3c6d940e3fa24627858ab18520544eda
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-03 00:33:55 -0600

    Add media urls mapping.

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.contrib import admin, settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +9,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('gym_app.urls')), # ADD THIS NEW TUPLE!
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
