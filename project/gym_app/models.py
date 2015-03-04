17f00364790e7f89616ec7feb4c2c2f98a789709
Devin Dvorak <devin.dvorak@yahoo.com>
2015-03-03 22:39:36 -0600

    Account Abstract Class
    Adding the base class for all users to inherit from and also just
    testing/messing around with GitHub to become familiar with it

--- project/gym_app/models.py
+++ project/gym_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#Abstract class that all user accounts will inherit from
+class Account(models.Model):
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=32)
+    email = models.EmailField(max_length=64)
+
+    class Meta:
+        abstract = True
+
