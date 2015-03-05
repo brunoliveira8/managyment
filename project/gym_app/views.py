d8ff4f0f9db9c1442b49178c0921f79f9d99fa05
Pentead0 <pent@Alefs-MacBook-Pro.local>
2015-03-04 18:15:05 -0600

    Add Register View

--- project/gym_app/views.py
+++ project/gym_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from gym_app.forms import UserForm
 
 
 # Create your views here.
@@ -15,3 +16,52 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
 
     return render(request, 'gym_app/index.html', context_dict)
+
+def register(request):
+
+    # A boolean value for telling the template whether the registration was successful.
+    # Set to False initially. Code changes value to True when registration succeeds.
+    registered = False
+
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm and UserProfileForm.
+        user_form = UserForm(data=request.POST)
+
+        # If the two forms are valid...
+        if user_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
+
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
+
+            # Now sort out the UserProfile instance.
+            # Since we need to set the user attribute ourselves, we set commit=False.
+            # This delays saving the model until we're ready to avoid integrity problems.
+            #profile = profile_form.save(commit=False)
+            #profile.user = user
+
+
+            # Update our variable to tell the template registration was successful.
+            registered = True
+
+        # Invalid form or forms - mistakes or something else?
+        # Print problems to the terminal.
+        # They'll also be shown to the user.
+        else:
+            print user_form.errors
+
+    # Not a HTTP POST, so we render our form using two ModelForm instances.
+    # These forms will be blank, ready for user input.
+    else:
+        user_form = UserForm()
+        #profile_form = UserProfileForm()
+
+    # Render the template depending on the context.
+    return render(request,
+            'gym_app/register.html',
+            {'user_form': user_form, 'registered': registered} )    
