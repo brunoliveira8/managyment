9e27327d942b576551671c9800cea2d772ce1b08
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-04 21:51:39 -0600

    Remove Account

--- project/gym_app/models.py
+++ project/gym_app/models.py
@@ -2,20 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-#Abstract class that all user accounts will inherit from
-class Account(models.Model):
-    firstName = models.CharField(max_length=32)
-    lastName = models.CharField(max_length=32)
-    username = models.CharField(max_length=32, unique=True)
-    password = models.CharField(max_length=32)
-    email = models.EmailField(max_length=64)
-
-    class Meta:
-        abstract = True
-
-class RegularAthlete(Account):
-    goalWeight = models.IntegerField(max_length=4)
-
 
 class WeightProgress(models.Model):
     startDate = models.DateField(auto_now_add=True)
