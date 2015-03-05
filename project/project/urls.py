aef6acea5903d89d5c0d3c37e0a7ad7936a5e603
Pentead0 <pent@Alefs-MacBook-Pro.local>
2015-03-04 18:15:51 -0600

    Url changes

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf import settings 
+from django.conf import settings
+from gym_app.views import index, register
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('gym_app.urls')), # ADD THIS NEW TUPLE!
+    url(r'^register/', register), # ADD THIS NEW TUPLE!
 )
 
 
