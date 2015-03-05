d574855d76940cdf94fdc407a729d268052b4367
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-04 18:26:13 -0600

    Model User created

--- project/gym_app/models.py
+++ project/gym_app/models.py
@@ -1,20 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-#Abstract class that all user accounts will inherit from
-class Account(models.Model):
-    firstName = models.CharField(max_length=32)
-    lastName = models.CharField(max_length=32)
-    username = models.CharField(max_length=32, unique=True)
-    password = models.CharField(max_length=32)
-    email = models.EmailField(max_length=64)
-
-    class Meta:
-        abstract = True
-
 #Class to make and test the login/logout system 
-class User(Account):
-    
+class User(models.Model):
+    user = models.OneToOneField(User) #Inheritance of User model
+
     def __unicode__(self):      #For Python 2, use __str__ on Python 3
-        return self.username
+        return self.user.username
