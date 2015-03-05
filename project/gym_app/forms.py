e03e6e56af2040c24198c9d69ff93dcbf3df4636
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-05 02:03:39 -0600

    Implements edit personal data and change password

--- project/gym_app/forms.py
+++ project/gym_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from gym_app.models import User
+from gym_app.models import User, RegularAthlete
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -13,4 +13,26 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'last_name', 'first_name')
+
+class UserEditForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserEditForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ChangePasswordForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('password', )
+
+class RegularAthleteForm(forms.ModelForm):
+
+    class Meta:
+        model = RegularAthlete
+        fields = ('goalWeight',)
