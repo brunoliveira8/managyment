942804a287f6451e23d707d5d056f73d878e3cf5
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-03-04 20:17:58 -0600

    Help_text disabled

--- project/gym_app/forms.py
+++ project/gym_app/forms.py
@@ -5,6 +5,12 @@ from gym_app.models import User
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'password']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
