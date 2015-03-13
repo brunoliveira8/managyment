9842e82c4cda7e43894b9f6a20c82ecf08312bba
sadallo andere neto <sadallo@hotmail.com>
2015-03-12 19:42:32 -0500

    Invalid login message

--- project/gym_app/views.py
+++ project/gym_app/views.py
@@ -129,8 +129,8 @@ def user_login(request):
                 return HttpResponse("Your account is disabled.")
         else:
             # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+            return render(request, 'gym_app/login.html', {'invalid': True })
+            #return HttpResponse("Invalid login details supplied.")
 
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
