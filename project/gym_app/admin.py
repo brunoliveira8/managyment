61909686132143db127deb31d582e44f5b198729
sadallo andere neto <sadallo@hotmail.com>
2015-04-14 20:11:00 -0500

    Remove permission to edit tables

--- project/gym_app/admin.py
+++ project/gym_app/admin.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
-from gym_app.models import Task, Athlete, PersonalTrainer, BodyScreening, WorkoutPlan, Tracker, MailBox, Message
+from gym_app.models import Task
 from django.contrib.auth.models import Permission
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(Permission)
-admin.site.register(Athlete)
-admin.site.register(PersonalTrainer)
-admin.site.register(BodyScreening)
-admin.site.register(WorkoutPlan)
-admin.site.register(Tracker)
-admin.site.register(MailBox)
-admin.site.register(Message)
 
 
 
